14d17181269579139714e5420d4fe7f95ddfa311
kevinreiner <kevin.reiner@fraud0.com>
2025-02-24 14:42:06 +0100

    docs

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  
+    base: './', build:{outDir:"docs/"},
+});
