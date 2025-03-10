946112c1fa44d47ccf945cbf1c55d7d28d0bc36b
kevinreiner <kevin.reiner@fraud0.com>
2025-03-10 11:14:33 +0100

    Burger menu fixed

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
-const menuBars = document.getElementById ('menu-bars');
+const menuBars = document.getElementById('menu-bars');
+const myNav = document.getElementById('my-nav');
 
 function toggleNav() {
-    menuBars.classList.toggle('change');
+  // Toggle the 'change' class for the menu bars animation
+  menuBars.classList.toggle('change');
+  
+  // Toggle the navigation height
+  if (myNav.style.height === "100%") {
+    myNav.style.height = "6%";
+  } else {
+    myNav.style.height = "100%";
+  }
 }
 
-// Event Listeners
-
+// Event Listeners - just one is needed now
 menuBars.addEventListener('click', toggleNav);
