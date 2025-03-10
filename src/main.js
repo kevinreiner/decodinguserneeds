7bc1e95dbb939b66a5ee1b0fddb2918d0e3c541c
kevinreiner <kevin.reiner@fraud0.com>
2025-03-10 11:28:06 +0100

    burger menu fix

--- src/main.js
+++ src/main.js
@@ -1,17 +1,44 @@
-const menuBars = document.getElementById('menu-bars');
-const myNav = document.getElementById('my-nav');
+// const menuBars = document.getElementById('menu-bars');
+// const myNav = document.getElementById('my-nav');
 
-function toggleNav() {
-  // Toggle the 'change' class for the menu bars animation
-  menuBars.classList.toggle('change');
+// function toggleNav() {
+//   // Toggle the 'change' class for the menu bars animation
+//   menuBars.classList.toggle('change');
   
-  // Toggle the navigation height
-  if (myNav.style.height === "100%") {
-    myNav.style.height = "6%";
-  } else {
-    myNav.style.height = "100%";
-  }
-}
+//   // Toggle the navigation height
+//   if (myNav.style.height === "100%") {
+//     myNav.style.height = "6vh";
+//   } else {
+//     myNav.style.height = "100%";
+//   }
+// }
 
-// Event Listeners - just one is needed now
-menuBars.addEventListener('click', toggleNav);
+// // Event Listeners - just one is needed now
+// menuBars.addEventListener('click', toggleNav);
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuBars = document.getElementById('menu-bars');
+    const myNav = document.getElementById('my-nav');
+  
+    let navOpen = false; // Flag, um den aktuellen Zustand der Navigation zu speichern
+  
+    function toggleNav() {
+      // Toggle der 'change'-Klasse für die Animation der Menü-Bars
+      menuBars.classList.toggle('change');
+      
+      // Navigation-Höhe wechseln basierend auf dem Zustand
+      if (navOpen) {
+        myNav.style.height = "6vh";
+      } else {
+        myNav.style.height = "100%";
+      }
+      
+      // Zustand umkehren
+      navOpen = !navOpen;
+    }
+  
+    // Event Listener für Klick- und Touch-Events hinzufügen
+    menuBars.addEventListener('click', toggleNav);
+    menuBars.addEventListener('touchstart', toggleNav);
+  });
